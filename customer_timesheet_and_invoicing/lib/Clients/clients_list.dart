6fa53fd5db16d045511dbcba46fe658ec2393c1f
NightLocke15 <christinefrances.holt@gmail.com>
2026-01-27 18:18:07 +0200

    Application Setup | main Structure set up

---
+++ customer_timesheet_and_invoicing/lib/Clients/clients_list.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class ClientList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Center();
+  }
+}
---
+++ customer_timesheet_and_invoicing/lib/Setup/setup_page.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class SetupPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+
+    return Center();
+  }
+}
